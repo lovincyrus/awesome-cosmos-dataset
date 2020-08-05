501460c02ef3c8832d72cc4189db5176d31bb491
Cyrus Goh <hello@lovincyrus.com>
2020-08-05 16:12:09 -0700

    prepend slug to projects

--- dataset/script/add.sh
+++ dataset/script/add.sh
@@ -4,6 +4,9 @@ DIR_PATH=dataset/projects/*.json
 for f in $DIR_PATH ; do \
   echo "Running add.sh"
 
-  # to append: +=
-  jq '. += {"languages": ["go","javascript","rust","none"],"is_ibc_support": false}' $f  | sponge $f ; \
+  # prepend
+  # jq '. |= {"slug": "app-name"} + .'  $f  | sponge $f ; \
+
+  # append: +=
+  # jq '. += {"languages": ["go","javascript","rust","none"],"is_ibc_support": false}' $f  | sponge $f ; \
 done;
